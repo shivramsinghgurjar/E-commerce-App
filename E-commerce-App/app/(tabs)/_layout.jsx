acb6fac96b6b3b032b8c9be3d6b7f7be6b111190
Shreya Singh <86734942+Ash1412005Fame@users.noreply.github.com>
2025-05-27 19:48:45 +0530

    Shreya: App Font Update 33:51

--- E-commerce-App/app/(tabs)/_layout.jsx
+++ E-commerce-App/app/(tabs)/_layout.jsx
@@ -1,13 +1,33 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
-
+// import { Colors } from 'react-native/Libraries/NewAppScreen'
+import Ionicons from '@expo/vector-icons/Ionicons'; 
+import {Colors} from './../../constants/Colors'
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{headerShown:false}}>
-      <Tabs.Screen name='home'/>
-      <Tabs.Screen name='explore'/>
-      <Tabs.Screen name='profile'/>
+    <Tabs screenOptions={{headerShown:false,
+      tabBarActiveTintColor:Colors.PRIMARY
+    }}>
+      <Tabs.Screen name='home'
+      options={{
+        tableLable:'Home',
+        tabBarIcon:({color})=><Ionicons name="home"
+         size={24} color={color} />
+      }}/>
+      
+      <Tabs.Screen name='explore'
+      options={{
+        tableLable:'Explore',
+        tabBarIcon:({color})=><Ionicons name="search"
+         size={24} color={color} />
+      }}/>
+      <Tabs.Screen name='profile'
+      options={{
+        tableLable:'Profile',
+        tabBarIcon:({color})=><Ionicons name="people-circle"
+         size={24} color={color} />
+      }}/>
     </Tabs>
   )
 }
