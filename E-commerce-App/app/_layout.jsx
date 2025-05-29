2332321bf3a1e2f1cc3f4dfa42fe8301d5b39446
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-05-29 10:47:31 +0530

    Shivram: Authentication:error code

---
+++ E-commerce-App/app/_layout.jsx
@@ -0,0 +1,27 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import React from "react";
+import LoginScreen from './../components/LoginScreen'
+export default function RootLayout() {
+  useFonts({
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
+  })
+  return (
+      <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <SignedIn>
+          <Stack screenOptions={{
+            headerShown:false
+          }}>
+            <Stack.Screen name="(tabs)"/>
+          </Stack>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen/>
+        </SignedOut>
+      </ClerkProvider>
+      
+  );
+}
