8b34274d7b92930b1f667253011468dc84573bce
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-05-30 13:52:48 +0530

    Shivram: Authentication 45:51

--- E-commerce-App/app/_layout.jsx
+++ E-commerce-App/app/_layout.jsx
@@ -2,38 +2,27 @@ import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import React from "react";
-import LoginScreen from "../components/LoginScreen";
-import Constants from "expo-constants";
-
+import LoginScreen from './../components/LoginScreen'
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    "outfit": require("./../assets/fonts/Outfit-Regular.ttf"),
-    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
-    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    // Return null or a loading component until fonts are loaded
-    return null;
-  }
-
-  const { clerkPublishableKey } = Constants.expoConfig.extra;
-
-  if (!clerkPublishableKey) {
-    console.error("Clerk publishableKey is missing. Check your app.config.js or app.json.");
-    return null; // Or show a fallback UI
-  }
-
+  useFonts({
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
+  })
   return (
-    <ClerkProvider publishableKey={clerkPublishableKey}>
-      <SignedIn>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </SignedIn>
-      <SignedOut>
-        <LoginScreen />
-      </SignedOut>
-    </ClerkProvider>
+      <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <SignedIn>
+          <Stack screenOptions={{
+            headerShown:false
+          }}>
+            <Stack.Screen name="(tabs)"/>
+          </Stack>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen/>
+        </SignedOut>
+      </ClerkProvider>
+      
   );
 }
+
--- E-commerce-App/components/LoginScreen.jsx
+++ E-commerce-App/components/LoginScreen.jsx
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+// import {Colors} from './../../constants/Colors'
 
 export default function LoginScreen() {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <View style={{
+        display:'flex',
+        alignItems:'center',
+        margin:100
+      }}>
+        <Image source={require('./../assets/images/login.jpg')} 
+          style={{
+            width:220,
+            height:450,
+            borderRadius:20,
+            borderWidth:6,
+            borderColor:'#000'
+          }}
+        />
+      </View>
+
+      <View style={{backgroundColor:'#fff', padding:20}}>
+        <Text>Your Ultimate 
+          <Text style={{
+            color:Colors.PRIMARY
+          }}>E-commerce</Text> App</Text>
+      </View>
     </View>
   )
 }
