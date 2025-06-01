06200c4f8f413036352255f59268195e0fec97d1
Shreya Singh <86734942+Ash1412005Fame@users.noreply.github.com>
2025-06-01 11:54:34 +0530

    HomePage Slider update 1:23:42

--- E-commerce-App/app/(tabs)/home.jsx
+++ E-commerce-App/app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 export default function home() {
   return (
     <View>
@@ -8,7 +9,7 @@ export default function home() {
        <Header/>
      }
      {
-      
+      <Slider/>
      }
      {
 
--- E-commerce-App/components/Home/Header.jsx
+++ E-commerce-App/components/Home/Header.jsx
@@ -1,63 +1,60 @@
 import { View, Text , Image ,TextInput,TextStyle} from 'react-native'
-
+import { useUser } from '@clerk/clerk-expo';
 import React from 'react'
 import {Colors} from './../../constants/Colors'
-import { Ionicons } from '@expo/vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 export default function Header() {
-    // const {user}=useUser();
+  const {user} =useUser();
   return (
-    <View>
-        <Text style={{fontSize:40,fontFamily:'outfit-bold'}}>home</Text>
-    </View>
-    /*<View style={{
-        padding:20,
-        paddingTop:40,
-        backgroundColor:Colors.PRIMARY,
-        borderBottomLeftRadius:20,
-        borderBottomRightRadius:20
-
+    <View style={{
+      padding:20,
+      paddingTop:40,
+      backgroundColor:Colors.PRIMARY,
+      borderBottomLeftRadius:20,
+      borderBottomRightRadius:20
     }}>
       <View style={{
         display:'flex',
         flexDirection:'row',
-        alignItem:'center',
+        alignItems:'center',
         gap:10
-      }}>
-        {/* { <Image source={{uri:user?.imageUrl}}
+      }}> 
+        <Image source={{uri:user?.imageUrl}}
         style={{
-            width:45,
-            height:45,
-            bordreRadius:99
-        }}></Image> } }
-        <View>
-            <Text style={{
-                color:'#fff'
-            }}>Welcome,</Text>
-            <Text style={{
-                fontSize:19,
-                fontFamily:'outfit-medium'
-            }}>user?.fullName</Text>
-        </View>
-        </View>
-        <View style={{
-            display:'flex',
-            flexDirection:'row',
-            gap:10,
-            alignItem:'center',
-            backgroundColor:'#fff',
-            padding:10,
-            marginVertical:10,
-            marginTop:15,
-            BorderRadius:8
-        }}>
-           <Ionicons name="search" size={24} color={Colors.PRIMARY} />
-           <TextInput placeholder='Search...'/>
-           style={{
-            fontFamily:'outfit',
-            fontSize:16
-           }}
-        </View>
+          width:45,
+          height:45,
+          borderRadius:99
+        }}/>
+
+      <View>
+        <Text style={{
+          color:'#fff',
+        }}>Welcome,</Text>
+        <Text style={{
+          fontSize:19,
+          fontFamily:'outfit-medium',
+          color:'#fff',
+        }}>{user?.fullName}</Text>
+      </View>
+      </View>
+      {
+        /* Search Bar */
+      }
+      <View style={{
+        display:'flex',
+        flexDirection:'row',
+        gap:10,
+        alignItems:'center',
+        backgroundColor:'#fff',
+        padding:5,
+        marginVertical:10,
+        marginTop:15,
+        borderRadius:8
+      }}>
+        <Ionicons name="search" size={24} color={Colors.PRIMARY}/> 
+        <TextInput placeholder='Search ...'/>
+      </View>
     </View>
-    */
+
   )
 }
