25fe7a954a8e55609774337dc677087885c63ee3
Shreya Singh <86734942+Ash1412005Fame@users.noreply.github.com>
2025-06-01 12:21:32 +0530

    shreya updated: initial Category section 1:27:22

--- E-commerce-App/app/(tabs)/home.jsx
+++ E-commerce-App/app/(tabs)/home.jsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
+import Category from '../../components/Home/Category'
 export default function home() {
   return (
     <View>
@@ -12,7 +13,7 @@ export default function home() {
       <Slider/>
      }
      {
-
+      <Category/>
      }
      {
 
