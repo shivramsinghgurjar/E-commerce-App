19feb643c8515b1c694398beb80d4c21e53ea84f
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-06-04 04:55:18 +0530

    Shivram: Business Review Section 03:09:00

--- E-commerce-App/app/businessdetail/[businessid].jsx
+++ E-commerce-App/app/businessdetail/[businessid].jsx
@@ -24,7 +24,7 @@ export default function BusinessDetail() {
     const docSnap=await getDoc(docRef);
     if(docSnap.exists())
     {
-      setBusiness(docSnap.data());
+      setBusiness({id:docSnap.id,...docSnap.data()});
       setLoading(false)
     }  
     else
--- E-commerce-App/components/BusinessDetail/Review.jsx
+++ E-commerce-App/components/BusinessDetail/Review.jsx
@@ -1,12 +1,32 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, ToastAndroid, FlatList, Image } from 'react-native'
 import React from 'react'
 import {Rating} from 'react-native-ratings'
 import { useState } from 'react'
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {Colors} from './../../constants/Colors'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../config/FirebaseConfig';
+import {useUser} from '@clerk/clerk-expo'
 
 export default function Review({business}) {
     const [rating,setRating]=useState(4);
+    const [userInput,setUserInput]=useState();
+    const {user} = useUser()  
+
+    const onSubmit=async()=>{
+      const docRef = doc(db,'E-commerceList', business?.id)
+      await updateDoc(docRef,{
+        reviews:arrayUnion({
+          rating:rating,
+          comment:userInput,
+          userName:user?.fullName,
+          userImage:user?.imageUrl,
+          userEmail:user?.primaryEmailAddress?.emailAddress
+        })
+      })
+      ToastAndroid.show('Comment Added Successfully !', ToastAndroid.BOTTOM)
+    }
   return (
     
     <View style={{
@@ -16,7 +36,7 @@ export default function Review({business}) {
       <Text style={{ 
         fontSize: 20,
          fontFamily:'outfit-bold' }}>Review</Text>
-         <View >
+         <View>
         <Rating
         imageSize={30}
             showRating={false}
@@ -26,6 +46,7 @@ export default function Review({business}) {
             <TextInput 
             placeholder='Write your comment'
             numberOfLines={4}
+            onChangeText={(value)=>setUserInput(value)}
             style={{
            borderWidth:1,
            padding:10,
@@ -35,9 +56,62 @@ export default function Review({business}) {
             minHeight:100
            }}
            />
-           <TouchableOpacity>
-            
+           <TouchableOpacity 
+           disabled={!userInput}
+           onPress={()=>onSubmit()}
+           style={{
+            padding:10,
+            backgroundColor:Colors.PRIMARY,
+            borderRadius:6,
+            marginTop:10
+           }}>
+
+            <Text style={{
+              fontFamily:'outfit',
+              color:'#fff',
+              textAlign:'center'
+            }}>Submit</Text>
            </TouchableOpacity>
+         </View>
+
+         {/* Display Previous Reviews */}
+
+         <View>
+          {business?.reviews?.map((item,index)=>(
+            <View style={{
+              display:'flex',
+              flexDirection:'row',
+              gap:10,
+              alignItems:'center',
+              padding:10,
+              borderWidth:1,
+              borderBlockColor:Colors.GREY,
+              borderRadius:15,
+              marginTop:10
+            }}>
+              <Image source={{uri:item.userImage}}
+                style={{
+                  width:50,
+                  height:50,
+                  borderRadius:99
+                }}
+              />
+              <View style={{
+                display:'flex',
+                gap:5
+              }}>
+                <Text style={{
+                  fontFamily:'outfit-medium'
+                }}>{item.userName}</Text>
+                <Rating
+                  imageSize={30}
+                  ratingCount={item.rating}
+                  style={{alignItems:'flex-start'}}
+                />
+                <Text>{item.comment}</Text>
+              </View>
+            </View>
+          ))}
          </View>
     </View>
   )
