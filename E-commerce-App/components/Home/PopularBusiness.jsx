c40e5c347fdf75d5a0829ae6d20854c49ed5f7ad
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-06-04 05:13:36 +0530

    Shivram: Quick Updates On Popular Business Section In home page

--- E-commerce-App/components/Home/PopularBusiness.jsx
+++ E-commerce-App/components/Home/PopularBusiness.jsx
@@ -17,8 +17,8 @@ export default function PopularBusiness() {
         const querySnapshot=await getDocs(q);
         querySnapshot.forEach((doc)=>{
             console.log(doc.data());
-            setBusinessList(prev=>[...prev,doc.data()])
-        })
+            setBusinessList(prev=>[...prev,{id:doc.id,...doc.data()}])
+        }) 
     }
   return (
     <View>
--- E-commerce-App/components/Home/PopularBusinessCard.jsx
+++ E-commerce-App/components/Home/PopularBusinessCard.jsx
@@ -1,10 +1,14 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import {Colors} from './../../constants/Colors'
+import { useRouter } from 'expo-router';
 
 export default function PopularBusinessCard({business}) {
+  const router = useRouter();
   return (
-    <View style={{
+    <TouchableOpacity 
+    onPress={()=>router.push("/businessdetail/"+business.id)}
+    style={{
         marginLeft:20,
         padding:10,
         backgroundColor:'#fff',
@@ -52,6 +56,6 @@ export default function PopularBusinessCard({business}) {
             }}>{business.category}</Text>
             </View>
       </View>
-    </View>
+    </TouchableOpacity>
   )
 }
