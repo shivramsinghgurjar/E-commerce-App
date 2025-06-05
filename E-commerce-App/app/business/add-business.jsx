d16456ecbe3a66afba921f59ba6a458a849dc553
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-06-05 18:57:29 +0530

    Shivram: Add New Business 4:05:25

---
+++ E-commerce-App/app/business/add-business.jsx
@@ -0,0 +1,158 @@
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from 'expo-router'
+import { Colors } from '../../constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
+import RNPickerSelect from 'react-native-picker-select';
+import {db} from './../../config/FirebaseConfig'
+import { collection, getDocs, query } from 'firebase/firestore';
+
+export default function AddBusiness() {
+    const navigation = useNavigation();
+    const [image, setImage] = useState(null);
+    const [categoryList,setCategoryList]=useState([]);
+    useEffect(() => {
+        navigation.setOptions({
+            headerTitle: 'Add New Business',
+            headerShown: true
+        })
+        GetCategoryList();
+    }, [])
+
+    const onImagePick = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1,
+        });
+        setImage(result?.assets[0].uri);
+        console.log(result);
+    }
+
+    const GetCategoryList=async()=>{
+        setCategoryList([]);
+        const q = query(collection(db,'Category'))
+        const snapShot=await getDocs(q);
+        snapShot.forEach((doc)=>{
+            console.log(doc.data());
+            setCategoryList(prev=>[...prev,{
+                label:(doc.data()).name,
+                value:(doc.data()).name
+            }])
+        })
+    }
+    return (
+        <View style={{
+            padding: 20
+        }}>
+            <Text style={{
+                fontFamily: 'outfit-bold',
+                fontSize: 25
+            }}>Add New Business</Text>
+            <Text style={{
+                fontFamily: 'outfit',
+                color: Colors.GREY
+            }}>Fill all details in order to add new business</Text>
+
+            <TouchableOpacity style={{
+                marginTop: 20
+            }}
+                onPress={() => onImagePick()}
+            >
+                {!image ? <Image source={require('./../../assets/images/placeholder.png')}
+                    style={{
+                        width: 100,
+                        height: 100
+                    }}
+                />
+                    :
+                    <Image source={{ uri: image }}
+                        style={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: 15
+                        }}
+                    />}
+            </TouchableOpacity>
+
+            <View>
+                <TextInput placeholder='Name'
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        fontSize: 17,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY,
+                        fontFamily: 'outfit'
+                    }}
+                />
+                <TextInput placeholder='Address'
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        fontSize: 17,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY,
+                        fontFamily: 'outfit'
+                    }}
+                />
+                <TextInput placeholder='Contact'
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        fontSize: 17,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY,
+                        fontFamily: 'outfit'
+                    }}
+                />
+                <TextInput placeholder='Email'
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        fontSize: 17,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY,
+                        fontFamily: 'outfit'
+                    }}
+                />
+                <TextInput placeholder='About'
+                    multiline
+                    numberOfLines={5}
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        fontSize: 17,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY,
+                        fontFamily: 'outfit',
+                        height: 100
+                    }}
+                />
+
+                <View style={{
+                        borderWidth: 1,
+                        borderRadius: 5,
+                        backgroundColor: '#fff',
+                        marginTop: 10,
+                        borderColor: Colors.PRIMARY
+                    }}>
+                    <RNPickerSelect
+                        onValueChange={(value) => console.log(value)}
+                        items={categoryList}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
--- E-commerce-App/components/Profile/MenuList.jsx
+++ E-commerce-App/components/Profile/MenuList.jsx
@@ -1,6 +1,7 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import {Colors} from './../../constants/Colors'
+import {useRouter} from 'expo-router'
 
 export default function MenuList() {
 
@@ -9,7 +10,7 @@ export default function MenuList() {
             id:1,
             name:'Add Business',
             icon:require('./../../assets/images/AddBusiness.jpeg'),
-            path:''
+            path:'/business/add-business'
         },
         {
             id:2,
@@ -30,6 +31,12 @@ export default function MenuList() {
             path:''
         }
     ]
+
+    const router=useRouter();
+
+    const onMenuClick=(item)=>{
+        router.push(item.path)
+    }
   return (
     <View style={{
         marginTop:50
@@ -38,7 +45,9 @@ export default function MenuList() {
         data={menuList}
         numColumns={2}
         renderItem={({item,index})=>(
-            <View style={{
+            <TouchableOpacity
+            onPress={()=>onMenuClick(item)}
+            style={{
                 display:'flex',
                 flexDirection:'row',
                 alignItems:'center',
@@ -62,7 +71,7 @@ export default function MenuList() {
                     fontSize:15,
                     flex:1
                 }}>{item.name}</Text>
-            </View>
+            </TouchableOpacity>
         )}
       />
 
